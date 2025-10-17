9dcd5c19c8aa44dc0ff02dc6fff89af0592c2831
Toni Penya-Alba <toni.penya.alba@gmail.com>
2025-10-17 11:12:48 +0200

    Move animation to GPU

--- main.js
+++ main.js
@@ -10,68 +10,31 @@ const sessionSeconds = params.get("session_duration_m")
 let breathElement = document.getElementById("breath");
 let progressElement = document.getElementById("progress");
 
-function inhale() {
-    console.info("Inhale");
-    breathElement.classList.add("breath-in");
-    breathElement.classList.remove("breath-out");
-}
-
-function exhale() {
-    console.info("Exhale");
-    breathElement.classList.add("breath-out");
-    breathElement.classList.remove("breath-in");
-}
-
 class Breather {
     constructor(breathSeconds, sessionSeconds) {
         console.info(
             `Created Breather with breathSeconds=${breathSeconds}, sessionSeconds=${sessionSeconds}`
         );
-        this.breathSeconds = breathSeconds;
         this.sessionSeconds = sessionSeconds;
-        this.isInhaling = true;
-        this.intervalId = null;
-        this.timeoutId = null;
-        this.startTime = null;
-        breathElement.style.transitionDuration = `${this.breathSeconds}s`;
-        progressElement.style.transitionDuration = `${this.breathSeconds}s`;
+
+        document.documentElement.style.setProperty(
+            "--breath-duration",
+            `${breathSeconds}s`
+        );
+        document.documentElement.style.setProperty(
+            "--session-duration",
+            `${this.sessionSeconds}s`
+        );
     }
 
     start() {
-        this.stop(); // Prevent starting multiple sessions
-        this.startTime = Date.now();
-        this.updateProgress();
-        breathElement.classList.remove("breath-off");
-        inhale();
+        progressElement.style.width = "100%"; // Trigger CSS animation
+        breathElement.classList.remove("paused");
 
-        this.intervalId = setInterval(() => {
-            this.isInhaling = !this.isInhaling;
-            this.isInhaling ? inhale() : exhale();
-            this.updateProgress();
-        }, this.breathSeconds * 1000);
-
-        this.timeoutId = setTimeout(() => {
+        setTimeout(() => {
             console.info("Session complete");
-            this.stop();
+            breathElement.classList.add("paused");
         }, this.sessionSeconds * 1000);
-    }
-
-    updateProgress() {
-        const elapsed = (Date.now() - this.startTime) / 1000;
-        const elapsed_after_animation = elapsed + this.breathSeconds;
-        const progress = Math.min(
-            (elapsed_after_animation / this.sessionSeconds) * 100,
-            100
-        );
-        progressElement.style.width = `${progress}%`;
-    }
-
-    stop() {
-        clearInterval(this.intervalId);
-        clearTimeout(this.timeoutId);
-        breathElement.classList.add("breath-off");
-        breathElement.classList.remove("breath-in");
-        breathElement.classList.remove("breath-out");
     }
 }
 
